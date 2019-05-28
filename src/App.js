247ffaeb36117da46b353304338a7659c4a00173
dek-haji <deeeqhaji@gmail.com>
2019-05-28 13:39:44 -0500

    added dBcalls and also fetch method calls in our applicationViews

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return ();
+  return (
+   <h1>Who let the Dog out</h1>
+  )
 }
 
 export default App;
---
+++ src/modules/dbCalls.js
@@ -0,0 +1,57 @@
+// const remoteURL = "http://localhost:5002"
+const eventsURL = "http://localhost:5002/events"
+const articlesURL = "http://localhost:5002/articles"
+const messagesURL = "http://localhost:5002/messages"
+const tasksURL = "http://localhost:5002/tasks"
+const usersURL = "http://localhost:5002/users"
+const friendsURL = "http://localhost:5002/friends"
+
+export default Object.create(null, {
+    get: {
+        value: function (id, URL) {
+            /*
+                Since the purpose of this module is to be used by
+                all of the more specialized one, then the string
+                of `animals` should not be hard coded here.
+            */
+            return fetch(`${URL}/${id}`).then(e => e.json())
+        }
+    },
+    all: {
+        value: function (URL) {
+            return fetch(`${URL}`).then(e => e.json())
+        }
+    },
+    delete: {
+        value: function (id, URL) {
+            return fetch(`${URL}/${id}`, {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }).then(e => e.json())
+        }
+    },
+    post: {
+        value: function (newObj, URL) {
+            return fetch(`${URL}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newObj)
+            }).then(data => data.json())
+        }
+    },
+    put: {
+        value: function (editObj, URL) {
+            return fetch(`${URL}/${editObj.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(editObj)
+            }).then(data => data.json());
+        }
+    }
+})
