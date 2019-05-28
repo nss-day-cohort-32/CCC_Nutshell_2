13ff8b7b39ecd74f817a34cd6128d7aef11eeb04
dek-haji <deeeqhaji@gmail.com>
2019-05-28 14:04:28 -0500

    fixed appviews error

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,57 @@
-import { Route, Redirect } from 'react-router-dom'
-import React, { Component } from "react"
-import { withRouter } from 'react-router'
+import React, { Component } from 'react';
 
-class ApplicationViews extends {
+class ApplicationViews extends Component {
 
+    state = {
+        articles: [],
+        events: [],
+        tasks: [],
+        messages: [],
+        friends: [],
+        users: []
 }
+
+
+componentDidMount() {
+    const newState = {}
+
+    fetch("http://localhost:5002/articles")
+        .then(r => r.json())
+        .then(articles => newState.articles = articles)
+        .then(() => fetch("http://localhost:5002/events")
+        .then(r => r.json()))
+        .then(events => newState.events = events)
+        .then(() => fetch("http://localhost:5002/tasks")
+        .then(r => r.json()))
+        .then(tasks => newState.tasks = tasks)
+
+        .then(() => fetch("http://localhost:5002/messages")
+        .then(r => r.json()))
+        .then(messages => newState.messages = messages)
+
+        .then(() => fetch("http://localhost:5002/friends")
+        .then(r => r.json()))
+        .then(friends => newState.friends = friends)
+
+        .then(() => fetch("http://localhost:5002/users")
+        .then(r => r.json()))
+        .then(users => newState.users = users)
+        .then(() => this.setState(newState))
+}
+
+
+
+
+
+
+
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default ApplicationViews;
