85c35e2371d6fb41c37340b931838d3deb0e81a6
Jason Collum <jasoncollum@gmail.com>
2019-05-29 08:31:49 -0500

    add event list output

---
+++ src/components/events/EventItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class EventItem extends Component {
+    render() {
+        return (
+            <div key={this.props.event.id} className="card">
+                <div className="card-body">
+                    <h3>{this.props.event.name}</h3>
+                    <p>{this.props.event.date}</p>
+                    <p>{this.props.event.location}</p>
+                    <button
+                        type="button"
+                        className="btn btn-success btn--edit"
+                        onClick={() => {
+                            this.props.history.push(`/events/${this.props.event.id}/edit`);
+                        }}>
+                        Edit
+                    </button>
+                    <button onClick={this.handleClick}>Delete</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default EventItem
