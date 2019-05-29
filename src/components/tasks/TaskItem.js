4efa80948bb55ee0bf343602954f13f95537a24f
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-05-29 14:27:34 -0500

    created checkbox, working functionality

--- src/components/tasks/TaskItem.js
+++ src/components/tasks/TaskItem.js
@@ -4,14 +4,19 @@ import Button from '@material-ui/core/Button';
 import CardContent from '@material-ui/core/CardContent';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+// import "./Task.css"
 
-// const Checkbox = props => (
-//     <input type="checkbox" {...props} />
-// )
+const Checkbox = props => (
+    <input type="checkbox" {...props} />
+)
 
 export default class TaskItem extends Component {
 
-    // state = { checked: false }
+    state = { checked: false }
+
+    // handleCheckboxChange = () => {
+    //     if()
+    // }
 
 
     render() {
@@ -19,10 +24,10 @@ export default class TaskItem extends Component {
             <div key={this.props.task.id} className="card">
                 <CardActions>
                     <Card className="card-body">
-                        <CardContent><h3>{this.props.task.task}</h3></CardContent>
-                        {/* <Checkbox
+                        <Checkbox className="taskCheckBox"
                             checked={this.state.checked}
-                            onChange={this.handleCheckboxChange}></Checkbox> */}
+                            onChange={this.handleCheckboxChange}></Checkbox>
+                        <CardContent><h3>{this.props.task.task}</h3></CardContent>
                         <Link className="nav-link" to={`/articles/${this.props.task.id}`}>Details</Link>
                         <Button size="small" color="primary"
                             type="button"
