1d8a5f8af6808a626bbbbdf18069142cc8ffa4a3
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-05-31 16:20:17 -0500

    add and delete working

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react'
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import TextField from '@material-ui/core/TextField';
-import MessageItem from "./MessageItem"
+// import React, { Component } from 'react'
+// import Button from '@material-ui/core/Button';
+// import Card from '@material-ui/core/Card';
+// import TextField from '@material-ui/core/TextField';
+// import MessageItem from "./MessageItem"
 
-export default class MessageForm extends Component {
-    render() {
-        return (
-            <div>
-                <form className="messageForm">
-                    <div className="form-group">
+// export default class MessageForm extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <form className="messageForm">
+//                     <div className="form-group">
 
-                        <TextField
-                            type="text"
-                            required
-                            className="form-control"
-                            onChange={this.handleFieldChange}
-                            id="content"
-                            placeholder="Type a message...."
-                        />
-                    </div>
-                    <Button variant="outlined" color="primary" size="large"
-                        type="submit"
-                        onClick={this.constructNewMessage}
-                        className="btn btn-primary"
-                    >
-                        Send Message
-                    </Button>
-                </form>
-            </div>
-        )
-    }
-}
+//                         <TextField
+//                             type="text"
+//                             required
+//                             className="form-control"
+//                             onChange={this.handleFieldChange}
+//                             id="content"
+//                             placeholder="Type a message...."
+//                         />
+//                     </div>
+//                     <Button variant="outlined" color="primary" size="large"
+//                         type="submit"
+//                         onClick={this.constructNewMessage}
+//                         className="btn btn-primary"
+//                     >
+//                         Send Message
+//                     </Button>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
